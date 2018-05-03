a84f72569c8686ff6c2153a1b6fa331183693048
joshua vickers <11vickersj@utcsheffield.org.uk>
2018-05-03 15:50:52 +0100

    Idents

--- mqqt_send.py
+++ mqqt_send.py
@@ -6,8 +6,11 @@ broker="broker.hivemq.com"
 broker="iot.eclipse.org"
 #define callback
 def on_message(client, userdata, message):
-time.sleep(1)
-print("received message =",str(message.payload.decode("utf-8")))
+    time.sleep(1)
+    print("received message =",str(message.payload.decode("utf-8")))
+
+
+
 client= paho.Client("client-001") #create client object client1.on_publish = on_publish #assign function to callback client1.connect(broker,port) #establish connection client1.publish("house/bulb1","on")
 ######Bind function to callback
 client.on_message=on_message
